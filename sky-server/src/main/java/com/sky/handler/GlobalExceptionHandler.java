6b0f4b7d5f1fe7a7c068699fed8e4a6005d04baa
forty2 <hereisb612@gmail.com>
2024-12-08 22:01:26 +0800

    add Username already exists Exception Handler

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -15,13 +18,29 @@ public class GlobalExceptionHandler {
 
     /**
      * 捕获业务异常
+     *
      * @param ex
      * @return
      */
     @ExceptionHandler
-    public Result exceptionHandler(BaseException ex){
+    public Result exceptionHandler(BaseException ex) {
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
     }
 
+    /*
+    Error updating database.
+    Cause: java.sql.SQLIntegrityConstraintViolationException:
+    Duplicate entry 'ethan2' for key 'employee.idx_username'
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException e) {
+        String message = e.getMessage();
+        if (message.contains("Duplicate entry")) {
+            String[] msgs = message.split(" ");
+            return Result.error(msgs[2] + MessageConstant.ALREADY_EXISTS);
+        } else {
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+    }
 }
