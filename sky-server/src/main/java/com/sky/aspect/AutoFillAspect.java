9141b360d5cfa1be66dbe6d6c4abca2a93e4adf9
hereisb612 <hereisb612@gmail.com>
2024-12-12 17:44:18 +0800

    clarify previous commits' code AutoFill

--- sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
+++ sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
@@ -10,6 +10,7 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+
 import java.lang.reflect.Method;
 import java.time.LocalDateTime;
 
@@ -25,34 +26,26 @@ public class AutoFillAspect {
     @Before("autoFillPointCut()")
     public void before(JoinPoint joinPoint) throws Exception {
 
-        // TODO clarify
-
         // method
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         Method method = signature.getMethod();
-        log.info(method.getName());
 
         // update or insert
         AutoFill annotation = method.getAnnotation(AutoFill.class);
         OperationType value = annotation.value();
 
+        // get pojo
         Object[] args = joinPoint.getArgs();
-        if (args == null && args.length == 0) {
+        if (args == null || args.length == 0) {
             return;
         }
         Object object = args[0];
-        log.info(object.toString());
-        log.info(object.getClass().getName());
 
+        // get setter
         Method setCreateTime = object.getClass().getDeclaredMethod("setCreateTime", LocalDateTime.class);
         Method setCreateUser = object.getClass().getDeclaredMethod("setCreateUser", Long.class);
         Method setUpdateTime = object.getClass().getDeclaredMethod("setUpdateTime", LocalDateTime.class);
         Method setUpdateUser = object.getClass().getDeclaredMethod("setUpdateUser", Long.class);
-
-        log.info(setCreateTime.toString());
-        log.info(setCreateUser.toString());
-        log.info(setUpdateTime.toString());
-        log.info(setUpdateUser.toString());
 
         if (value == OperationType.INSERT) {
             setCreateTime.setAccessible(true);
